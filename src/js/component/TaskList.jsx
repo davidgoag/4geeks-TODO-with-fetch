43ba99c44372a2f5f9fb89e3e7515b8a796646b8
David Gómez <davidgoag1@gmail.com>
2021-09-28 00:38:44 +0000

    JSX Working

---
+++ src/js/component/TaskList.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export const TaskList = () => {
+	const [task, setTask] = React.useState("");
+	const [list, setList] = React.useState([]);
+	const handleInput = e => {
+		if (e.keyCode == 13) {
+			setTask(e.target.value);
+			setList([...list, task]);
+			setTask("");
+		}
+	};
+	const deleteToDo = indexToRemove => {
+		let filterList = list.filter((taskToRemove, i) => i != indexToRemove);
+		setList(filterList);
+	};
+
+	return (
+		<div className="page">
+			<div className="">
+				<div>
+					<input
+						type="text"
+						placeholder="What needs to be done?"
+						value={task}
+						onChange={event => setTask(event.target.value)}
+						onKeyDown={e => handleInput(e)}
+					/>
+				</div>
+				<div>
+					<ul>
+						{list.map((singleTask, i) => {
+							return (
+								<li key={i}>
+									{singleTask}
+									<span onClick={() => deleteToDo(i)}>
+										<strong> x</strong>
+									</span>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
